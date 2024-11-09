bb3a03de91df7645891ac94136d31c4761d6bf99
Andrii Pesliak <apesliak@piraeusbank.ua>
2024-11-09 20:09:19 +0200

    Add generating account number for new account.

--- src/main/java/spaceurgent/banking/service/impl/AccountServiceImpl.java
+++ src/main/java/spaceurgent/banking/service/impl/AccountServiceImpl.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import spaceurgent.banking.model.Account;
 import spaceurgent.banking.repository.AccountRepository;
 import spaceurgent.banking.service.AccountService;
+import spaceurgent.banking.utils.AccountNumberGenerator;
 
 import java.math.BigDecimal;
 
@@ -18,6 +19,7 @@ public class AccountServiceImpl implements AccountService {
     @Override
     public Account createAccount(BigDecimal initialBalance) {
         requireNonNull(initialBalance, "Initial balance is required");
-        return accountRepository.save(new Account(initialBalance));
+        final var accountNumber = AccountNumberGenerator.nextAccountNumber();
+        return accountRepository.save(new Account(accountNumber, initialBalance));
     }
 }
