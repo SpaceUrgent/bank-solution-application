8cb9e999810755abe534a1a9e6ece40bb32c0087
Andrii Pesliak <apesliak@piraeusbank.ua>
2024-11-09 02:30:56 +0200

    Remove static contructor for Currency enum.

--- src/main/java/spaceurgent/banking/model/Currency.java
+++ src/main/java/spaceurgent/banking/model/Currency.java
@@ -1,19 +1,5 @@
 package spaceurgent.banking.model;
 
-import spaceurgent.banking.exception.UnsupportedCurrencyException;
-
-import java.util.Arrays;
-
-import static java.util.Objects.requireNonNull;
-
 public enum Currency {
     UAH;
-
-    public static Currency of(String currencyAbbreviation) {
-        requireNonNull(currencyAbbreviation, "Currency abbreviation is required");
-        return Arrays.stream(Currency.values())
-                .filter(currency -> currency.name().equals(currencyAbbreviation))
-                .findFirst()
-                .orElseThrow(() -> new UnsupportedCurrencyException("Currency '%s' is not supported".formatted(currencyAbbreviation)));
-    }
 }
