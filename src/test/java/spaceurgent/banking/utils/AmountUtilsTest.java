6d10856fc9622863a049aba453dc56a0b8455027
Andrii Pesliak <apesliak@piraeusbank.ua>
2024-11-10 21:20:37 +0200

    Refactore amount utils test.

--- src/test/java/spaceurgent/banking/utils/AmountUtilsTest.java
+++ src/test/java/spaceurgent/banking/utils/AmountUtilsTest.java
@@ -1,5 +1,6 @@
 package spaceurgent.banking.utils;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import spaceurgent.banking.Constants;
@@ -13,7 +14,8 @@ class AmountUtilsTest {
 
     @ParameterizedTest
     @ValueSource(doubles = {0.001, 0.00111, 0.009})
-    void round(Double doubleValue) {
+    @DisplayName("Round - OK")
+    void round_ok(Double doubleValue) {
         final var roundTarget = BigDecimal.valueOf(doubleValue);
         final var expected = roundTarget.setScale(Constants.DEFAULT_AMOUNT_SCALE, RoundingMode.FLOOR);
         assertEquals(expected, AmountUtils.round(roundTarget));
@@ -21,24 +23,28 @@ class AmountUtilsTest {
 
     @ParameterizedTest
     @ValueSource(doubles = {-0.001, -0.00111, -0.009, -1, -100})
+    @DisplayName("Is negative returns true")
     void isNegative_returnsTrue(Double doubleValue) {
         assertTrue(AmountUtils.isNegative(BigDecimal.valueOf(doubleValue)));
     }
 
     @ParameterizedTest
     @ValueSource(doubles = {0, 0.001, 0.009, 1, 100})
+    @DisplayName("Is negative returns false")
     void isNegative_returnsFalse(Double doubleValue) {
         assertFalse(AmountUtils.isNegative(BigDecimal.valueOf(doubleValue)));
     }
 
     @ParameterizedTest
     @ValueSource(doubles = {0, 0.001, 0.009, -0.001, -0.00111, -0.009, -1, -100})
+    @DisplayName("Is negative or 0 returns true")
     void isNegativeOrZero_returnsTrue(Double doubleValue) {
         assertTrue(AmountUtils.isNegativeOrZero(BigDecimal.valueOf(doubleValue)));
     }
 
     @ParameterizedTest
     @ValueSource(doubles = {0.01, 1, 100})
+    @DisplayName("Is negative or 0 returns false")
     void isNegativeOrZero_returnsFalse(Double doubleValue) {
         assertFalse(AmountUtils.isNegativeOrZero(BigDecimal.valueOf(doubleValue)));
     }
