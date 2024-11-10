b61c6027343238e5d0c041bce052672f6ead8cd1
Andrii Pesliak <apesliak@piraeusbank.ua>
2024-11-10 22:38:04 +0200

    Make text constatns class final.

--- src/test/java/spaceurgent/banking/TestConstants.java
+++ src/test/java/spaceurgent/banking/TestConstants.java
@@ -1,6 +1,9 @@
 package spaceurgent.banking;
 
-public class TestConstants {
+public final class TestConstants {
+    private TestConstants() {
+    }
+
     public static final String TEST_ACCOUNT_NUMBER = "26000000000000";
     public static final String DEFAULT_SOURCE_ACCOUNT_NUMBER = "26000000000001";
     public static final String DEFAULT_TARGET_ACCOUNT_NUMBER = "26000000000002";
