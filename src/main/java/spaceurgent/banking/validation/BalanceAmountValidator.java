3b155124a614d483f5c9d38e7d211f37e225003e
Andrii Pesliak <apesliak@piraeusbank.ua>
2024-11-10 21:29:08 +0200

    Refactor validators.

--- src/main/java/spaceurgent/banking/validation/BalanceAmountValidator.java
+++ src/main/java/spaceurgent/banking/validation/BalanceAmountValidator.java
@@ -6,13 +6,15 @@ import spaceurgent.banking.exception.ValidationException;
 import java.math.BigDecimal;
 import java.util.Objects;
 
+import static spaceurgent.banking.utils.AmountUtils.isNegative;
+
 @Component
 public class BalanceAmountValidator implements Validator<BigDecimal> {
 
     @Override
     public void validate(BigDecimal balanceAmount) {
         Objects.requireNonNull(balanceAmount, "Balance amount is required");
-        if (balanceAmount.compareTo(BigDecimal.ZERO) < 0) {
+        if (isNegative(balanceAmount)) {
             throw new ValidationException("Invalid balance. Balance must be equal or greater than 0");
         }
     }
--- src/main/java/spaceurgent/banking/validation/TransferAmountValidator.java
+++ src/main/java/spaceurgent/banking/validation/TransferAmountValidator.java
@@ -4,9 +4,9 @@ import org.springframework.stereotype.Component;
 import spaceurgent.banking.exception.ValidationException;
 
 import java.math.BigDecimal;
-import java.math.RoundingMode;
 
 import static java.util.Objects.requireNonNull;
+import static spaceurgent.banking.utils.AmountUtils.isNegativeOrZero;
 
 @Component
 public class TransferAmountValidator implements Validator<BigDecimal> {
@@ -14,7 +14,7 @@ public class TransferAmountValidator implements Validator<BigDecimal> {
     @Override
     public void validate(BigDecimal transferAmount) {
         requireNonNull(transferAmount, "Transfer amount is required");
-        if (transferAmount.setScale(2, RoundingMode.FLOOR).compareTo(BigDecimal.ZERO) <= 0) {
+        if (isNegativeOrZero(transferAmount)) {
             throw new ValidationException("Invalid transfer amount. Amount must be grater than 0");
         }
     }
